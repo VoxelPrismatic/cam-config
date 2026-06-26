f3b27ae64038ad391508bdcad6c99587d5a85571
PRIZ ;] <voxelprismatic@pm.me>
2026-06-25 22:20:52 -0500

    Remove sudo/pkexec escalation; app now runs as normal user (v4l2loopback preloaded via akmod)

--- main.go
+++ main.go
@@ -2,54 +2,10 @@ package main
 
 import (
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
-	"syscall"
-
-	"golang.org/x/term"
 
 	"voxelprismatic/cam-config/cam"
 )
 
-func escalate() {
-	var cmd *exec.Cmd
-	if term.IsTerminal(0) {
-		args := []string{"-E"}
-		args = append(args, os.Args...)
-		cmd = exec.Command("sudo", args...)
-	} else {
-		file, err := filepath.Abs(os.Args[0])
-		if err != nil {
-			panic(err)
-		}
-		os.Args[0] = file
-
-		args := []string{"env"}
-		for _, key := range os.Environ() {
-			if !strings.Contains(key, " ") {
-				args = append(args, key)
-			}
-		}
-
-		args = append(args, os.Args...)
-		cmd = exec.Command("pkexec", args...)
-	}
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	if err != nil {
-		panic(err)
-	}
-	os.Exit(0)
-}
-
 func main() {
-	if syscall.Geteuid() != 0 {
-		escalate()
-		return
-	}
-
 	cam.RunGUI(os.Args)
 }
